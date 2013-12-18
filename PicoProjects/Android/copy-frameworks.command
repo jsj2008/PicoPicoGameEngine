18383bc7130b664cebe9663eeae1c68e52414a65
Hiromitsu Yamaguchi <yamagame@mac.com>
2013-12-18 15:38:07 +0900

    deleted をコミット

--- PicoProjects/Android/copy-frameworks.command
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd `dirname $0`
-
-cp ../../FullBuild/Android/libs/armeabi/* ./jni/armeabi/
-cp ../../FullBuild/Android/libs/armeabi-v7a/* ./jni/armeabi-v7a/
-cp ../../FullBuild/Android/libs/x86/* ./jni/x86/
--- PicoProjects/Android/jni/Application.mk
+++
@@ -1,6 +0,0 @@
-# it is needed for ndk-r5
-LOCAL_ARM_MODE := arm
-LOCAL_ARM_NEON := true
-APP_ABI := armeabi armeabi-v7a x86
-APP_STL := stlport_static
-APP_MODULES := prebuild-cocos2d prebuild-cocosdenshion prebuild-freetype prebuild-lua prebuild-box2d prebuild-flmml prebuild-picogame prebuild-picomain
